df6a130e4fc983e4b9350e0465df828719b31ecf
BrunoCostaPrado <202203973541@alunos.estacio.br>
2024-02-20 16:12:23 -0300

    feat:ui base

---
+++ src/App.tsx
@@ -0,0 +1,114 @@
+import { FileDown, MoreHorizontal, Plus, Search } from "lucide-react"
+import { useQuery } from "@tanstack/react-query"
+import { Header } from "./components/header"
+import { Tabs } from "./components/tabs"
+import { Button } from "./components/ui/button"
+import { Control, Input } from "./components/ui/input"
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from "./components/ui/table"
+import { Pagination } from "./components/pagination"
+// import { Header, Tabs } from "$components"
+
+export interface TagResponse {
+	first: number
+	prev: number | null
+	next: number
+	last: number
+	pages: number
+	items: number
+	data: Tag[]
+}
+
+export interface Tag {
+	title: string
+	amountOfVideos: number
+	id: string
+}
+
+function App() {
+	const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+		queryKey: ["get-tags"],
+		queryFn: async () => {
+			const response = await fetch()
+			// `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`
+			const data = await response.json()
+			// delay 2s
+			await new Promise((resolve) => setTimeout(resolve, 2000))
+
+			return data
+		}
+	})
+
+	return (
+		<div className="py-10 space-y-8">
+			<div>
+				<Header />
+				<Tabs />
+			</div>
+			<main className="max-w-6xl mx-auto space-x-5">
+				<div className="flex items-center gap-3">
+					<h1 className="text-xl font-bold">Tags</h1>
+					<Button variant="primary">
+						<Plus className="size-3" />
+						Create New
+					</Button>
+				</div>
+				<div className="flex items-center justify-between">
+					<Input variant="filter">
+						<Search className="size-3" />
+						<Control placeholder="Search tags..." />
+					</Input>
+
+					<Button>
+						<FileDown className="size-3" />
+						Export
+					</Button>
+				</div>
+
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead></TableHead>
+							<TableHead>Tag</TableHead>
+							<TableHead>Amount of videos</TableHead>
+							<TableHead></TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{Array.from({ length: 10 }).map((value, index) => {
+							return (
+								<TableRow key={index}>
+									<TableCell>
+										<div className=" flex flex-col gap-0.5">
+											<span className="font-medium">React</span>
+											<span className="text-xs text-zinc-500">
+												5a623c10-906e-42cf-b2df-e56c9feb35c2
+											</span>
+										</div>
+									</TableCell>
+									<TableCell className="text-zinc-300">13 videos</TableCell>
+									<TableCell className="text-right ">
+										<Button size="icon">
+											<MoreHorizontal className="size-4" />
+										</Button>
+									</TableCell>
+								</TableRow>
+							)
+						})}
+					</TableBody>
+				</Table>
+
+				
+				<Pagination/>
+			</main>
+		</div>
+	)
+}
+
+export { App }
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export * from "./header"
+export * from "./pagination"
+export * from "./tabs"
